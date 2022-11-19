07e79baa8de4518cc984e51b5102bdd3b832117d
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2022-11-19 11:35:52 +0530

    host error

--- util/database.js
+++ util/database.js
@@ -1,7 +1,9 @@
 const Sequelize = require('sequelize')
 require('dotenv').config()
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, process.env.HOST, {
-    
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+    host: process.env.HOST,
     dialect : 'mysql',
+    
+    
   });
 module.exports = sequelize
