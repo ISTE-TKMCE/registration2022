e99dc9bd0cc8a8098e44f0d2c8285336ea109210
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2022-11-19 10:52:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+let express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const sequelize=require('./util/database')
+
+var port = process.env.PORT || 5000;
+
+
+dotenv.config();
+
+
+//////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////
+
+
+
+let app = express();
+
+//Middlewares
+
+app.use(express.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
+
+
+//routes and logic
+
+
+app.get("/", (req, res) => {
+    res.send({ "name": "Vinay" });
+});
+
+sequelize.sync().then(result => {
+    app.listen(port, () => {
+        console.log("server started")
+    })
+}).catch(err=>console.log(err))
---
+++ util/database.js
@@ -0,0 +1,6 @@
+const Sequelize = require('sequelize')
+require('dotenv').config()
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+    dialect : 'mysql',
+  });
+module.exports = sequelize
