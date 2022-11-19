867f27252f2b0a909a5cf825ec0590d4d494e519
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2022-11-20 00:11:47 +0530

    db change

--- models/models.js
+++ models/models.js
@@ -1,6 +1,6 @@
 const Sequelize =  require('sequelize')
 const sequelize = require('../util/database')
-const User = sequelize.define('User', {
+const User = sequelize.define('Applicant', {
     id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
