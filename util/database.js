6d6609f8cdc27ba53ae532268bc9861d1e740d3e
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2022-11-19 11:31:00 +0530

    host

--- util/database.js
+++ util/database.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 require('dotenv').config()
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, process.env.HOST, {
+    
     dialect : 'mysql',
   });
 module.exports = sequelize
