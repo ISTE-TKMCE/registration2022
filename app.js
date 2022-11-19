d6af818211522e3cb69150f517cea7618c7e9e33
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2022-11-19 16:00:54 +0530

    put user logic

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 let express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-const sequelize=require('./util/database')
+const sequelize = require('./util/database')
 const morgan = require('morgan');
-
+const cors = require("cors")
+const multer = require("multer")
+const path = require("path")
+const User = require("./models/models").User
 var port = process.env.PORT || 5000;
 
 
@@ -20,7 +23,9 @@ dotenv.config();
 let app = express();
 
 //Middlewares
-
+const publicdrc = path.resolve(__dirname, 'public')
+app.use(express.static(publicdrc))
+app.use(cors())
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(
@@ -30,18 +35,63 @@ app.use(
 );
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-app.set("view engine", "ejs");
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/screenshots')
+    },
+    filename: (req, file, cb) => {
 
-
+        cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname)
+    }
+})
+const upload = multer({ storage });
 //routes and logic
 
-
+app.put("/user", upload.single('file'), async (req, res) => {
+    try {
+        const data = await JSON.parse(req.body.data)
+        const user = await User.findOne({ email: req.body.email }).then(response => { if (response) { return true } else { return false } }).catch(err => { return false })
+        if (user) {
+            return res.send({ status: 403, message: "Email already registered" })
+        }
+        else {
+            User.create({
+                name: data.name,
+                email: data.email,
+                phonenumber: data.phonenumber,
+                dob: data.dob,
+                branch: data.branch,
+                batch: data.batch,
+                year: data.year,
+                admissionyear: data.admissionyear,
+                admissionnumber: data.admissionnumber,
+                address: data.address,
+                specialinterests: data.specialinterests,
+                careerpreference: data.careerpreference,
+                typeofservice: data.typeofservice,
+                accholdersname: data.accholdersname,
+                transactionid: data.transactionid,
+                imageurl: new Date().toISOString().replace(/:/g, '-') + '-' + req.file.originalname
+            }).then(response => {
+                console.log(response)
+                res.send({ status: 200, message: "User created successfully" })
+            }).catch(err => {
+                console.log(err)
+                res.send({ status: 400, message: "Couldn't register user" })
+            })
+        }
+    }
+    catch (err) {
+        console.log(err)
+        res.send({ status: 400, message: "Couldn't register user" })
+    }
+})
 app.get("/", (req, res) => {
-    res.render("homepage");
+    res.send("Nothing to see");
 });
 
 sequelize.sync().then(result => {
     app.listen(port, () => {
         console.log("server started")
     })
-}).catch(err=>console.log(err))
+}).catch(err => console.log(err))
--- models/models.js
+++ models/models.js
@@ -15,8 +15,63 @@ const User = sequelize.define('User', {
         type: Sequelize.STRING,
         allowNull: false,
     },
-    password: {
+    phonenumber: {
         type: Sequelize.STRING,
         allowNull: false,
-    }
+    },
+    dob: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    branch: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    batch: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    year: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    admissionyear: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    admissionnumber: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    specialinterests: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    careerpreference: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    typeofservice: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    accholdersname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    transactionid: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    imageurl: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
 })
+module.exports= {
+    User:User,
+}
